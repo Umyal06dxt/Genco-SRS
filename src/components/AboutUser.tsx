dc5b25c5ab4f1284be38f95238fe58657a65c93d
Aditya <Adityapandeyadp@gmail.com>
2025-03-10 03:08:42 +0530

    great stuffffffffffff

--- src/components/AboutUser.tsx
+++ src/components/AboutUser.tsx
@@ -8,14 +8,14 @@ export default function Authors() {
                 "adityapandeyadp@gmail.com",
             name: "Aditya Dutt Pandey",
             designation: "12409188",
-            src: "https://media.licdn.com/dms/image/v2/D4D03AQG2_BgCnIfPtw/profile-displayphoto-shrink_400_400/B4DZV8bwXtHwAk-/0/1741549408332?e=1747267200&v=beta&t=jxRT5PMVhtSYBAiseO8K-T_rIgc-cF_nJgraLHzrrLY",
+            src: "/a.jpeg",
         },
         {
             quote:
                 "umyal06dixit@gmail.com",
             name: "Umyal Dixit",
             designation: "12417102",
-            src: "https://media.licdn.com/dms/image/v2/D4D03AQFBE4xffnM2ZA/profile-displayphoto-shrink_200_200/B4DZP6DbhFGkAY-/0/1735067024696?e=1747267200&v=beta&t=7wMqcVIUGC-FTaSHSqGI_XA--RbCfS01GW18VcWNey8",
+            src: "/u.jpeg",
         },
     ];
     return <AnimatedTestimonials testimonials={testimonials} />;
